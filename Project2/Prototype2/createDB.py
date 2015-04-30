ac6b10cfd8f82e3deed7467f08e1fd4dc9cf00ae
Juan Caballero <juan.caballero.perez@gmail.com>
2015-04-30 15:58:26 -0500

    error in SQL

--- Project2/Prototype2/createDB.py
+++ Project2/Prototype2/createDB.py
@@ -5,23 +5,20 @@ con.execute("CREATE TABLE teachers (id INTEGER PRIMARY KEY,   \
                                     email CHAR(100) NOT NULL, \
                                     affiliation CHAR(100),    \
                                     phone CHAR(20),           \
-                                    interest TEXT
-                                    )")
+                                    interest TEXT)")
 con.execute("CREATE TABLE students (id INTEGER PRIMARY KEY,   \
                                     name CHAR(100) NOT NULL,  \
                                     email CHAR(100) NOT NULL, \
                                     affiliation CHAR(100),    \
                                     phone CHAR(20),           \
                                     interest TEXT,            \
-                                    skills TEXT
-                                    )")
+                                    skills TEXT)")
 
 con.execute("CREATE TABLE jobs     (id INTEGER PRIMARY KEY,    \
                                     name CHAR(100) NOT NULL,   \
                                     description TEXT NOT NULL, \
                                     teacher INT,               \
                                     skills TEXT,               \
-                                    status BOOLEAN
-                                    )")
+                                    status BOOLEAN)")
                                     
 con.commit()
